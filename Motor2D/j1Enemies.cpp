8f2e555a1eb5548e07008316a9ea347d8327d05f
Roger Sanchez March <roger.sanchezmarch@hotmail.es>
2017-10-30 12:29:22 +0100

    Added coords

--- Motor2D/j1Enemies.cpp
+++ Motor2D/j1Enemies.cpp
@@ -25,26 +25,62 @@ j1Enemies::j1Enemies()
 	idle_left.PushBack({ 0, 66, 85, 64 });
 	idle_left.PushBack({ 94, 67, 85, 63 });
 	idle_left.PushBack({ 187, 67, 86, 62 });
+	idle_left.PushBack({ 281, 67, 88, 63 });
+	idle_left.PushBack({ 375, 67, 85, 63 });
+	idle_left.PushBack({ 469, 68, 84, 62 });
+	idle_left.PushBack({ 561, 68, 81, 62 });
 	idle_left.loop = true;
 	idle_left.speed = 0.07f;
 
-	walk_right.PushBack({ 0, 0, 0, 0});
+	walk_right.PushBack({ 0, 264, 93, 62});
+	walk_right.PushBack({ 93, 265, 93, 62 });
+	walk_right.PushBack({ 187, 262, 84, 64 });
+	walk_right.PushBack({ 281, 262, 78, 64 });
+	walk_right.PushBack({ 376, 262, 78, 64 });
+	walk_right.PushBack({ 467, 262, 83, 64 });
+	walk_right.PushBack({ 562, 263, 90, 63 });
 	walk_right.loop = true;
 	walk_right.speed = 0.07f;
 
-	walk_left.PushBack({ 0, 0, 0, 0 });
+	walk_left.PushBack({ 0, 327, 93, 62 });
+	walk_left.PushBack({ 93, 327, 93, 62 });
+	walk_left.PushBack({ 187, 327, 84, 64 });
+	walk_left.PushBack({ 281, 327, 78, 64 });
+	walk_left.PushBack({ 376, 327, 78, 64 });
+	walk_left.PushBack({ 467, 327, 83, 64 });
+	walk_left.PushBack({ 562, 327, 90, 63 });
 	walk_left.loop = true;
 	walk_left.speed = 0.07f;
 
-	jump_right.PushBack({ 0, 0, 0, 0 });
+	jump_right.PushBack({ 0, 132, 82, 62 });
+	jump_right.PushBack({ 93, 132, 88, 62 });
+	jump_right.PushBack({ 187, 132, 93, 63 });
+	jump_right.PushBack({ 281, 132, 94, 63 });
+	jump_right.PushBack({ 375, 132, 92, 63 });
+	jump_right.PushBack({ 467, 132, 94, 63 });
+	jump_right.PushBack({ 562, 132, 92, 63 });
 	jump_right.loop = true;
 	jump_right.speed = 0.07f;
 
-	jump_left.PushBack({ 0, 0, 0, 0 });
+	jump_left.PushBack({ 0, 197, 82, 62 });
+	jump_left.PushBack({ 93, 197, 88, 62 });
+	jump_left.PushBack({ 187, 197, 93, 63 });
+	jump_left.PushBack({ 281, 197, 94, 63 });
+	jump_left.PushBack({ 375, 197, 92, 63 });
+	jump_left.PushBack({ 467, 197, 94, 63 });
+	jump_left.PushBack({ 562, 197, 92, 63 });
 	jump_left.loop = true;
 	jump_left.speed = 0.07f;
 
-	attack_right.PushBack({ 0, 0, 0, 0 });
+	attack_right.PushBack({ 0, 393, 88, 94 });
+	attack_right.PushBack({ 93, 393, 82, 94 });
+	attack_right.PushBack({ 187, 393, 79, 94 });
+	attack_right.PushBack({ 281, 393, 79, 94 });
+	attack_right.PushBack({ 375, 393, 84, 94 });
+	attack_right.PushBack({ 467, 393, 82, 94 });
+	attack_right.PushBack({ 561, 393, 85, 94 });
+
+
 	attack_right.loop = true;
 	attack_right.speed = 0.07f;
 
